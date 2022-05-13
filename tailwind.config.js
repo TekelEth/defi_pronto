4bc5506bb32361b22ca295620e6a8ea26cc38917
blockendsol <blockendsol@gmail.com>
2022-05-13 12:44:53 +0100

    added: font and theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				monument: ['Monument Extended', 'sans-serif'],
+			},
+			colors: {
+				theme: {
+					100: 'matrix(-1, 0, 0, 1, 0, 0)',
+					200: 'matrix(-1, 0, 0, 1, 0, 0)',
+					300: 'matrix(-1, 0, 0, 1, 0, 0)',
+					main: '#E74141',
+					dark: '#1A1A1A',
+					light: '#FFFFFF',
+				},
+			},
+		},
+	},
+	plugins: [],
+};
