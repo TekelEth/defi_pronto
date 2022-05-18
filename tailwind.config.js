7ed48c2d54bf7c36e7563cdc684040be733a144c
blockendsol <blockendsol@gmail.com>
2022-05-18 01:14:36 +0100

    added: navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	content: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				monument: ['Monument Extended', 'sans-serif'],
+				DMSans: ['DM Sans', 'sans-serif'],
+				monument: ['Monument Extended', 'cursive'],
 			},
 			colors: {
 				theme: {
